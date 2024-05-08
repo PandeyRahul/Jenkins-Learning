861e280ac6089c152e5854022f49649628601efe
Rahul Pandey <rahulrpandey1105@gmail.com>
2024-05-08 18:13:37 +0530

    Initial Commit

---
+++ test.py
@@ -0,0 +1,21 @@
+import importlib.metadata
+
+lib = ['fastapi',
+       'numpy',
+       'pandas',
+       'joblib',
+       'scikit-learn',
+       'scipy',
+       'pytest',
+       'setuptools',
+       'wheel',
+       'pydantic',
+       'uvicorn',
+       'gunicorn']
+
+data_dict = {}
+
+for i in lib:
+    data_dict[i] = importlib.metadata.version(i)
+
+print(data_dict)
